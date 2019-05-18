f742ba0fc98c08bbb6768d5d15471980a4c12c2d
jimmylu1 <jimmy88lu@gmail.com>
2019-05-17 18:21:59 -0700

    supertest passes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,18 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+class Navbar_sidebar extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>NAVBAR_SIDEBAR</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Navbar_sidebar />, document.getElementById("navbar_sidebar"));
